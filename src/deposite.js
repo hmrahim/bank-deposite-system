39a41e4564d5ab813f6b23ed5fdae813e865a9d5
hmrahim <h.m.rahimnet@gmail.com>
2022-02-10 04:29:20 +0600

    balance deposite done

--- src/deposite.js
+++ src/deposite.js
@@ -1,4 +1,16 @@
 document.getElementById("deposite-btn").addEventListener("click", function () {
     const depositeInput = document.getElementById("deposite-input")
-    console.log(depositeInput);
+    const depositeBalance = document.getElementById("deposite-balance")
+    const totalBalance = document.getElementById("total-balance")
+    
+    if (depositeInput.value != "") {
+        const totalDeposite = parseFloat(depositeBalance.innerText) + parseFloat(depositeInput.value) 
+        depositeBalance.innerText = totalDeposite
+        const newTotalBalance = parseFloat(totalBalance.innerText) + parseFloat(depositeInput.value)
+        totalBalance.innerText = newTotalBalance
+        depositeInput.value = ""
+      
+    } else {
+        alert("please provie a valid number")
+    }
 })
